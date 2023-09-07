a6d55191caa2b466368b23b0827d714d2b9a22e7
JangHJ <yg9316@swu.ac.kr>
2023-09-08 01:42:21 +0900

    인트로화면 오류 수정 및 manifest activity 수정

--- app/src/main/java/com/example/everysearch1/splashowl.kt
+++ app/src/main/java/com/example/everysearch1/splashowl.kt
@@ -7,11 +7,17 @@ import android.os.Handler
 
 class splashowl : AppCompatActivity() {
 
+    private val splashTimeOut: Long = 2000 // 2초 동안 스플래시 화면 표시
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashowl) // 스플래시 화면의 레이아웃 파일을 설정해야 합니다.
 
-        var intent = Intent(this, MainActivity::class.java)
+        Handler().postDelayed({
+            // 일정 시간 후에 MainActivity로 이동
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        finish()
+            finish() // 현재 액티비티 종료
+        }, splashTimeOut)
     }
 }
