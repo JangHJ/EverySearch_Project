7103288ac2453ed44c622de9e9cce81a9c80aec7
JangHJ <yg9316@swu.ac.kr>
2023-09-10 01:05:53 +0900

    Firebase 로그인 기능 수정1

--- app/src/main/java/com/example/everysearch1/MainActivity.kt
+++ app/src/main/java/com/example/everysearch1/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         NextBtn.setOnClickListener(){
             val nextIntent = Intent(this, findschoolActivity()::class.java)
             startActivity(nextIntent)
+            finish()
         }
     }
 
--- app/src/main/java/com/example/everysearch1/splashowl.kt
+++ app/src/main/java/com/example/everysearch1/splashowl.kt
@@ -4,19 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 class splashowl : AppCompatActivity() {
 
     private val splashTimeOut: Long = 2000 // 2초 동안 스플래시 화면 표시
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashowl) // 스플래시 화면의 레이아웃 파일을 설정해야 합니다.
 
+        val auth = FirebaseAuth.getInstance() // Firebase 인증 객체 초기화
+
         Handler().postDelayed({
-            // 일정 시간 후에 MainActivity로 이동
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            // 현재 로그인한 사용자 확인
+            val currentUser = auth.currentUser
+            currentUser?.email?.let { Log.d("로그인 확인 !!!!", it) }
+
+            // 사용자가 로그인한 경우 mainsearch 화면으로 이동
+            if (currentUser != null) {
+                val intent = Intent(this, mainsearch::class.java)
+                startActivity(intent)
+            } else {
+                // 사용자가 로그인하지 않은 경우 MainActivity로 이동
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
             finish() // 현재 액티비티 종료
         }, splashTimeOut)
     }
