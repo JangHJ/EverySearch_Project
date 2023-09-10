5965856272f44531eccab8d6b9abc7eb403c7f2e
JangHJ <yg9316@swu.ac.kr>
2023-09-10 20:40:09 +0900

    회원가입 기능 추가 및 수정

--- app/src/main/java/com/example/everysearch1/signup.kt
+++ app/src/main/java/com/example/everysearch1/signup.kt
@@ -7,56 +7,44 @@ import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import kotlinx.android.synthetic.main.activity_chooseschool_afterlogin.*
-import kotlinx.android.synthetic.main.activity_chooseschool_afterlogin.button17
-import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_signup.editEmail
+import kotlinx.android.synthetic.main.activity_signup.*
 
-class login : AppCompatActivity() {
+class signup : AppCompatActivity() {
+
     private var auth : FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_signup)
 
         auth = FirebaseAuth.getInstance()
 
-        //뒤로가기 버튼
         btnBack.setOnClickListener{
             finish()
         }
-        //로그인 버튼 클릭 시
-        btnLogin.setOnClickListener{
-            emailLogin()
-        }
         //회원가입 버튼 클릭 시
         btnSignup.setOnClickListener{
-            val nextIntent = Intent(this, signup()::class.java)
-            startActivity(nextIntent)
+            emailSignup()
         }
     }
-    private fun emailLogin() {
+    private fun emailSignup() {
         if (editEmail.text.toString().isNullOrEmpty() || editPwd.text.toString().isNullOrEmpty()) {
             Toast.makeText(this, "이메일과 비밀번호를 입력해주세요", Toast.LENGTH_LONG).show()
             return
         }
         var email = editEmail.text.toString()
         var password = editPwd.text.toString()
+
+        auth?.createUserWithEmailAndPassword(email, password)
         auth?.signInWithEmailAndPassword(email, password)
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    Toast.makeText(this, "Email 로그인 성공", Toast.LENGTH_LONG).show()
-                    moveMainPage(auth?.currentUser)
+                    Toast.makeText(this, "회원가입이 완료되었습니다", Toast.LENGTH_LONG).show()
+                    finish()
                 } else {
-                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "회원가입이 실패했습니다", Toast.LENGTH_LONG).show()
                 }
             }
-    }
-    private fun moveMainPage(user: FirebaseUser?) {
-        if (user != null) {
-            /*val after_Intent = Intent(this, navigationreplace()::class.java)
-            startActivity(after_Intent)*/
-            finish()
-        }
+
     }
 }
